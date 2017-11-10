496735a8be42b3fca1a2da6aafd5f6f2f1d2d6e6
Tim Lepage <timlepage9@gmail.com>
2017-11-10 16:18:27 +0100

    list

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+
+interface ResultatRecherche {
+  title:string;
+  author:string;
+  date:number;
+  image:string;
+}
+const RESULTAB : ResultatRecherche[]=[{
+  title: "titre", author: "author", date: 2017, image: "https://www.saint-mary.church/wp-content/uploads/2016/09/lorem-ipsum-logo.jpg"}];
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  resultat = RESULTAB;
   constructor(public navCtrl: NavController) {
 
   }
