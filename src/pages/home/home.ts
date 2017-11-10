32c982b6bbb862c98d36a1fc0134c83c0624c15b
Tim Lepage <timlepage9@gmail.com>
2017-11-10 16:59:29 +0100

    end

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -9,12 +9,19 @@ interface ResultatRecherche {
   image:string;
 }
 const RESULTAB : ResultatRecherche[]=[{
-  title: "titre", author: "author", date: 2017, image: "https://www.saint-mary.church/wp-content/uploads/2016/09/lorem-ipsum-logo.jpg"}];
+  title: "titre1", author: "author1", date: 2017, image: 'img src ="https://www.saint-mary.church/wp-content/uploads/2016/09/lorem-ipsum-logo.jpg"'},
+  {
+    title: "titre2", author: "author2", date: 2018, image: 'img src ="https://www.saint-mary.church/wp-content/uploads/2016/09/lorem-ipsum-logo.jpg"'
+  },
+  {
+    title: "titre3", author: "author3", date: 2019, image: 'img src ="https://www.saint-mary.church/wp-content/uploads/2016/09/lorem-ipsum-logo.jpg"'
+  }];
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
   resultat = RESULTAB;
   constructor(public navCtrl: NavController) {
