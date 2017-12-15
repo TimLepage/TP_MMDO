2047f2d30624d4460185230158fa5b8304039eee
Tim Lepage <timlepage9@gmail.com>
2017-12-15 15:33:32 +0100

    TP3 OK

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -4,7 +4,9 @@ import { DetailsPage } from '../details/details';
 import { Observable } from 'rxjs/Observable';
 import { HttpParams } from '@angular/common/http';
 import { HttpClient } from '@angular/common/http';
-
+import { AlertController } from 'ionic-angular';
+import { Subscription } from 'rxjs/Subscription';
+import { Shake } from '@ionic-native/shake';
 
 
 export interface ResultatRecherche {
@@ -13,6 +15,7 @@ export interface ResultatRecherche {
   release_date:number;
   poster_path:string;
 }
+
 
 const key: string = 'ebb02613ce5a2ae58fde00f4db95a9c1';
 
@@ -25,13 +28,15 @@ const key: string = 'ebb02613ce5a2ae58fde00f4db95a9c1';
 
 
 export class HomePage {
+  private shakeSubscription: Subscription;
+
   searchres: Observable<ResultatRecherche[]>=null;
   query : string = '';
   show_no_result : boolean = true;
   //constructor(public navCtrl: NavController) {
   //  }
 
-  constructor(public navCtrl: NavController, private http: HttpClient) { }
+  constructor(public navCtrl: NavController, private http: HttpClient, public alertCtrl: AlertController, private shake: Shake) { }
 
   onInput(): void {
     console.log(this.query);
@@ -54,4 +59,45 @@ export class HomePage {
       params: new HttpParams().set('api_key', key).set('query', this.query)
     }).pluck("results");
   }
+
+  discoverMovies(): Observable<ResultatRecherche[]>{
+    return this.http.get<ResultatRecherche[]>("https://developers.themoviedb.org/3/discover/movie-discover", {
+      params: new HttpParams().set('api_key', key).set('primary_release_year', "2018")
+    }).pluck("results");
+  }
+
+
+
+  showRandomMovieAlert(movies: ResultatRecherche[]): void{
+    var item = movies[Math.floor(Math.random() * movies.length)];
+    let confirm = this.alertCtrl.create({
+      title: item.title,
+      message: item.overview,
+      buttons: [
+        {
+          text: 'Cancel',
+        },
+        {
+          text: 'Details',
+          handler: () => {
+            this.push(item);
+          }
+        }
+      ]
+    });
+    confirm.present();
+  }
+
+
+  ionViewDidEnter(){
+    this.shakeSubscription = this.shake.startWatch().switchMap(()=>this.discoverMovies()).subscribe(movies=>this.showRandomMovieAlert(movies));
+  }
+  ionViewWillLeave(){
+    this.shakeSubscription.unsubscribe();
+  }
 }
+
+
+
+
+
